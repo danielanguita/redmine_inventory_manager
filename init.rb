183a2df6e6519524e921cbc1d408de11bd2ac2c9
Daniel Anguita <ask@me.com>
2014-12-13 12:47:30 -0300

    compatible with Redmine 2.x, version 0.9

--- init.rb
+++ init.rb
@@ -2,7 +2,7 @@ Redmine::Plugin.register :redmine_inventory_manager do
   name 'Redmine Inventory Manager Plugin'
   author 'Daniel Anguita O.'
   description 'Take your warehouse or office inventory on the same platform of your projects'
-  version '0.0.1'
+  version '0.9'
   url 'https://github.com/danielanguita/Redmine-Inventory-Manager'
 
   permission :inventory, {:inventory => [:index, :movements, :categories, :parts, :warehouses, :providors]}, :public => false
