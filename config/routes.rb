831dd44e329bf90ef01ee22a289a0861e9e83583
Emiliano Baum <ebaum@conae.gov.ar>
2024-06-23 17:47:55 +0000

    Cambiando el archivo routes.rb y agregando rutas mas especificas.

--- config/routes.rb
+++ config/routes.rb
@@ -7,13 +7,13 @@ get 'inventory(/:action(/:id))', controller: :inventory
 post 'inventory(/:action(/:id))', controller: :inventory
 put 'inventory(/:action(/:id))', controller: :inventory
 
-get 'inventory/categories/:id', to: 'inventory#categories'
+get 'inventory/categories/:id', controller: :inventory
 
 # get 'inventory(/categories:action(/:id))', controller: :inventory
 # post 'inventory(/categories:action(/:id))', controller: :inventory
 # put 'inventory(/categories:action(/:id))', controller: :inventory
 
-get 'inventory/parts/:id', to: 'inventory#parts'
+get 'inventory/parts/:id', controller: :inventory
 # get 'inventory(/parts:action(/:id))', controller: :inventory
 # post 'inventory(/parts:action(/:id))', controller: :inventory
 # put 'inventory(/parts:action(/:id))', controller: :inventory
