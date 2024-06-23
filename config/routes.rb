d4e37b83ffa31613c3d223c4a7143619867d8f85
Emiliano Baum <ebaum@conae.gov.ar>
2024-06-23 17:59:55 +0000

    Cambiando el archivo routes.rb y agregando rutas mas especificas.

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,27 @@
 # Plugin's routes
 # See: http://guides.rubyonrails.org/routing.html
 
-get 'inventory', :to => 'inventory#index'
-
-get 'inventory(/:action(/:id))', controller: :inventory
-post 'inventory(/:action(/:id))', controller: :inventory
-put 'inventory(/:action(/:id))', controller: :inventory
-
-get 'inventory(/categories:action(/:id))', controller: :inventory
-
-# get 'inventory(/categories:action(/:id))', controller: :inventory
-# post 'inventory(/categories:action(/:id))', controller: :inventory
-# put 'inventory(/categories:action(/:id))', controller: :inventory
-
-get 'inventory(/parts:action(/:id))', controller: :inventory
-# get 'inventory(/parts:action(/:id))', controller: :inventory
-# post 'inventory(/parts:action(/:id))', controller: :inventory
-# put 'inventory(/parts:action(/:id))', controller: :inventory
+Rails.application.routes.draw do
+    resources :inventory, controller: 'inventory', only: [] do
+      collection do
+        get 'reports'
+        get 'report_export'
+        get 'index'
+        get 'inventory_stock_xls'
+        get 'ajax_get_part_value'
+        get 'ajax_get_part_info'
+        get 'movements'
+        get 'categories'
+        get 'parts'
+        get 'providors'
+        get 'warehouses'
+      end
+  
+      member do
+        get 'edit_in', to: 'inventory#movements'
+        get 'edit_out', to: 'inventory#movements'
+      end
+    end
+  
+    root 'inventory#index' # Ajusta esto a la acción de controlador que desees como raíz del sitio
+  end  
